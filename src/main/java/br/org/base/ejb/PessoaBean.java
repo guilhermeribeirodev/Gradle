1c611402e9e06ada13efe89a9ad77c1388d35f0d
pis0195 <guilherme.ribeiro@supero.com.br>
2014-11-22 22:36:16 -0200

    first commit

---
+++ src/main/java/br/org/base/ejb/PessoaBean.java
@@ -0,0 +1,8 @@
+package br.org.base.ejb;
+
+/**
+ * Created by supero on 22/11/2014.
+ */
+
+public class PessoaBean {
+}
---
+++ src/main/java/br/org/base/model/Pessoa.java
@@ -0,0 +1,7 @@
+package br.org.base.model;
+
+/**
+ * Created by supero on 22/11/2014.
+ */
+public class Pessoa {
+}
